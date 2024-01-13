341333cdbf64ad059efbeaf9aae6fec68cd9d569
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 03:44:27 +0500

    NODEJS Assignment Q27
    27. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
    • If the alien is green, print a message that the player earned 5 points.
    • If the alien is yellow, print a message that the player earned 10 points.
    • If the alien is red, print a message that the player earned 15 points.
    • Write three versions of this program, making sure each message is printed for the appropriate color alien.

---
+++ Q27.ts
@@ -0,0 +1,38 @@
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+// • If the alien is yellow, print a message that the player earned 10 points.
+// • If the alien is red, print a message that the player earned 15 points.
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+// // alien is green
+// let alienColor: string = 'green'
+// if (alienColor === 'green') {
+//     console.log("player just earned 5 points!");
+// }
+// else if (alienColor === 'yellow') {
+//     console.log("player just earned 10 points");
+// }
+// else if(alienColor === 'red') {
+//     console.log("player just earned 15 points!");
+// }
+// alien is yellow
+// let alienColor: string = 'yellow'
+// if (alienColor === 'green') {
+//     console.log("player just earned 5 points!");
+// }
+// else if (alienColor === 'yellow') {
+//     console.log("player just earned 10 points");
+// }
+// else if(alienColor === 'red') {
+//     console.log("player just earned 15 points!");
+// }
+// alien is red
+let alienColor: string = 'red'
+if (alienColor === 'green') {
+    console.log("player just earned 5 points!");
+}
+else if (alienColor === 'yellow') {
+    console.log("player just earned 10 points");
+}
+else if(alienColor === 'red') {
+    console.log("player just earned 15 points!");
+}
